9a76a6fcea467311ee837e233d26faa138ce7329
Aseem Patni <me@aseempatni.com>
2015-01-19 08:12:40 +0530

    initial commit with first assignment part one

---
+++ ass1/fibchildpar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+void main () {
+	int n;
+	scanf("%d", &n);
+	int i, returnStatus;    
+	int first = 0, second = 1, next;
+	for (i=1; i<=n; i++) {
+		int  pid;
+		pid = fork();
+		if (pid == 0) {
+			if ( i==1 || i==2 )
+				next = 1;
+			else {
+				next = first + second;
+			}
+			printf("%d\n",next);
+			exit(next);
+		}
+		else {
+			waitpid(pid, &returnStatus, 0);
+			next = WEXITSTATUS(returnStatus);
+			first = second;
+			second = next;
+		}
+	}
+}
