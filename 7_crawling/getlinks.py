36a1ded26bb073d9abe5ab90d78f6d42d0d7cc7e
Aseem Patni <apatnip@gmail.com>
2015-04-14 18:21:46 +0530

    relative urls handled.

--- 7_crawling/getlinks.py
+++ 7_crawling/getlinks.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 import re
 import lxml.html
+import urlparse
+from urlparse import urljoin
+def is_absolute(url):
+    return bool(urlparse.urlparse(url).netloc)
+
 s=""
 while True:
     w = raw_input()
@@ -9,4 +14,7 @@ while True:
         break
 hxs = lxml.html.document_fromstring(s)
 links = hxs.xpath('//a/@href')
+for link in links:
+    if not is_absolute(link):
+        link = urljoin(s, link)
 print ','.join(links)
